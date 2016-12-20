b2cb2121517e16cb0fd21aaaa99995aa5f07427b
Ro-ee Tal <roeetal1@gmail.com>
2016-12-20 02:26:05 -0800

    board animation

--- scripts/mainscripts.js
+++ scripts/mainscripts.js
@@ -6,3 +6,24 @@ function toggleNav() {
       x.className = "main-navbar";
   }
 }
+
+function checkScroll() {
+  var videos = document.getElementsByTagName("video"),
+  fraction=1;
+  for(var i = 0; i < videos.length; i++) {
+    var video = videos[i];
+    video.loop = false;
+    video.addEventListener('ended',swap,false);
+    var x = document.getElementById("orbit");
+    if (x.className === "orbit init animate") {
+      video.play();
+    }
+  }
+}
+window.addEventListener('scroll', checkScroll, false);
+window.addEventListener('resize', checkScroll, false);
+
+function swap(){
+  var x = document.getElementById("orbit");
+  x.className += " completed";
+}
