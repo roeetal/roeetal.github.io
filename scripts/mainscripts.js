1e742d8230c1201255693c0f2fa2ca73a32f6b26
Ro-ee Tal <roeetal1@gmail.com>
2016-12-20 04:40:46 -0800

    board text and background changes

--- scripts/mainscripts.js
+++ scripts/mainscripts.js
@@ -27,3 +27,17 @@ function swap(){
   var x = document.getElementById("orbit");
   x.className += " completed";
 }
+
+function changeBackground() {
+  var images = ['first', 'second', 'third'],
+      i = 1; // Counter to keep track of the class index
+
+  // Cache the element reference
+  var $main = $('#about');
+  setInterval(function(){
+    var newClass = "about "+images[i++];
+    $main.attr('class', newClass); // Set the class attribute value
+    i = i % images.length; // If greater than class length, reset back to 0
+  }, 5000);
+}
+window.onload = changeBackground;
