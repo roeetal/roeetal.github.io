0f5c20a2af998ed51b856bec31b6c73b7d3eda9e
Ro-ee Tal <roeetal1@gmail.com>
2017-03-27 19:04:23 -0700

    sat page

--- js/main.js
+++ js/main.js
@@ -13,3 +13,25 @@
 //   }, 4000);
 // }
 // window.onload = changeBackground;
+
+
+function checkScroll() {
+  var videos = document.getElementsByTagName("video"),
+  fraction=1;
+  for(var i = 0; i < videos.length; i++) {
+    var video = videos[i];
+    video.loop = false;
+    video.addEventListener('ended',swap,false);
+    var x = document.getElementById("orbit");
+    if (x.className === "page init animate") {
+      video.play();
+    }
+  }
+}
+window.addEventListener('scroll', checkScroll, false);
+window.addEventListener('resize', checkScroll, false);
+
+function swap(){
+  var x = document.getElementById("orbit");
+  x.className += " completed";
+}
