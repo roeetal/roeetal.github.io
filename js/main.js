94af33effa51347090fa7b58e663fd3e8e927a49
Ro-ee Tal <roeetal1@gmail.com>
2017-09-18 01:27:12 -0700

    fixup and scss

--- js/main.js
+++ js/main.js
@@ -15,23 +15,23 @@
 // window.onload = changeBackground;
 
 
-function checkScroll() {
-  var videos = document.getElementsByTagName("video"),
-  fraction=1;
-  for(var i = 0; i < videos.length; i++) {
-    var video = videos[i];
-    video.loop = false;
-    video.addEventListener('ended',swap,false);
-    var x = document.getElementById("orbit");
-    if (x.className === "page init animate") {
-      video.play();
-    }
-  }
-}
-window.addEventListener('scroll', checkScroll, false);
-window.addEventListener('resize', checkScroll, false);
-
-function swap(){
-  var x = document.getElementById("orbit");
-  x.className += " completed";
-}
+// function checkScroll() {
+//   var videos = document.getElementsByTagName("video"),
+//   fraction=1;
+//   for(var i = 0; i < videos.length; i++) {
+//     var video = videos[i];
+//     video.loop = false;
+//     video.addEventListener('ended',swap,false);
+//     var x = document.getElementById("orbit");
+//     if (x.className === "page init animate") {
+//       video.play();
+//     }
+//   }
+// }
+// window.addEventListener('scroll', checkScroll, false);
+// window.addEventListener('resize', checkScroll, false);
+//
+// function swap(){
+//   var x = document.getElementById("orbit");
+//   x.className += " completed";
+// }
